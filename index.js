98dc86ee1ba0bc720bc76961e320a12547cea080
=Bi-Huynh <=huynhtrungduc1231998@gmail.com>
2020-11-30 11:50:35 +0700

    viet nut tim kiem

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const infomationStaff = [
+    { id: 1, name: 'Duc' },
+    { id: 2, name: 'Hung' }
+]
 
 app.set('view engine', 'pug');
 app.set("views", "./views");
 
-
-
 // trang chủ, thông tin nhân viên
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { infomationStaff: infomationStaff });
 });
 
+app.get('/search', (req, res) => {
+    let query = req.query.searchStaff;
+    let arrStaff = infomationStaff.filter(staff => staff.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+    res.render('index', { infomationStaff: arrStaff });
+})
+
+app.post('/')
 
 app.listen(port, () => {
     console.log(`start ${port}`);
