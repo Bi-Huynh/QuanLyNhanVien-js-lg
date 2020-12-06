64484b497d24a6a962606acef2079cb0de6f6f95
=Bi-Huynh <=huynhtrungduc1231998@gmail.com>
2020-12-06 12:12:43 +0700

    tach file theo mvc

--- views/main/index.js
+++ views/main/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const user = require('../user/user');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'pug');
 app.set("views", "./views");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.render('main/index');
--- views/user/user.js
+++ views/user/user.js
@@ -1,58 +1,22 @@
 const express = require('express');
-const db = require('../../Database/db');
-const bodyParser = require('body-parser');
+
+const controllerUser = require('../../controllers/user.controllers');
 
 const router = express.Router();
 
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: true }));
-
-const informationStaff = db.get('informationStaff');
-
 // trang chủ, thông tin nhân viên
-router.get('/', (req, res) => {
-    if (informationStaff.value().length == 0) {
-        res.render('user/index_user', {
-            _informationStaff: '',
-            _listStaff: {}
-        });
-    } else {
-        res.render('user/index_user', {
-            _informationStaff: '',
-            _listStaff: informationStaff.value()
-        });
-    }
-});
+router.get('/', controllerUser.indexUser);
 
-// tìm kiếm nhân viên theo ten
-router.get('/search', (req, res) => {
-    let query = req.query.searchStaff;
-    let arrStaff = informationStaff.value().filter(staff => {
-        return staff.nameStaff.toLowerCase().indexOf(query.toLowerCase()) !== -1
-    });
-    res.render('user/index_user', { _listStaff: arrStaff });
-})
+// tìm kiếm nhân viên theo tên
+router.get('/search', controllerUser.searchUser);
 
-router.get('/create_user', (req, res) => {
-    res.render('user/create_user');
-})
+
+router.get('/create_user', controllerUser.viewCreateUser);
 
 // bấm vô 1 nhân viên để xem thông tin nhân viên đó
-router.get('/:userID', (req, res) => {
-    // sử dụng địa chỉ có tên là 'Router parameters'
-    let userID = parseInt(req.params.userID);
-    let user = informationStaff.find({ id: userID }).value();
-    res.render('user/index_user', {
-        _informationStaff: user,
-        _listStaff: informationStaff.value()
-    });
-})
+router.get('/:userID', controllerUser.getUserID);
 
 // lưu thông tin nhân viên mới
-router.post('/create_user', (req, res) => {
-    newStaff = req.body;
-    informationStaff.push(newStaff).write();
-    res.redirect('/');
-})
+router.post('/create_user', controllerUser.postCreateUser);
 
 module.exports = router;
