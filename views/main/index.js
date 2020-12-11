efdf48e3619b4ef2c65a7bf0b33cf528e0a2c5bd
=Bi-Huynh <=huynhtrungduc1231998@gmail.com>
2020-12-11 13:39:48 +0700

    cai dat cookies

--- views/main/index.js
+++ views/main/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const user = require('../user/user');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = 3000;
@@ -10,6 +11,7 @@ app.set("views", "./views");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(express.static('views'));
 // phải có thằng này để nó có thể đọc các file css img ....
 
