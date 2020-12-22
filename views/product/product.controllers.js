ccaa19d46314ee20603b55961cbdaa64b7fa5b69
=Bi-Huynh <=huynhtrungduc1231998@gmail.com>
2020-12-22 12:56:34 +0700

    dang viet code giao dien

--- views/product/product.controllers.js
+++ views/product/product.controllers.js
@@ -3,5 +3,6 @@ const db = require('../../config/db');
 const products = db.get('products');
 
 module.exports.index = (req, res) => {
-    res.render('product/index_product');
+    // console.log(products.value());
+    res.render('product/index_product', { _listProducts: products.value() });
 }
