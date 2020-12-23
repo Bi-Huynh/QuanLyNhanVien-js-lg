40c49c10883d2edab92fa8dbd2dd5b052d5bd9dd
=Bi-Huynh <=huynhtrungduc1231998@gmail.com>
2020-12-23 17:35:45 +0700

    viet thanh phan trang, moi trang gom 8 san pham

--- views/product/product.controllers.js
+++ views/product/product.controllers.js
@@ -4,10 +4,14 @@ const products = db.get('products');
 
 module.exports.index = (req, res) => {
     let page = parseInt(req.params.page) || 1;
-    console.log(page);
     let perPage = 8;
     let start = (page - 1) * perPage;
     let end = page * perPage;
     let array = products.value().slice(start, end);
-    res.render('product/index_product', { _listProducts: array });
+    let length = parseInt(products.value().length / perPage);
+    res.render('product/index_product', {
+        _listProducts: array,
+        _page: page,
+        _length: length + 1
+    });
 }
