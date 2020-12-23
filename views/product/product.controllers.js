8851a15084851e46febf14ace6593a4e0710d24d
=Bi-Huynh <=huynhtrungduc1231998@gmail.com>
2020-12-23 16:44:57 +0700

    chinh router de thuc hien phan trang va load gia tri tuong ung

--- views/product/product.controllers.js
+++ views/product/product.controllers.js
@@ -3,6 +3,11 @@ const db = require('../../config/db');
 const products = db.get('products');
 
 module.exports.index = (req, res) => {
-    // console.log(products.value());
-    res.render('product/index_product', { _listProducts: products.value() });
+    let page = parseInt(req.params.page) || 1;
+    console.log(page);
+    let perPage = 8;
+    let start = (page - 1) * perPage;
+    let end = page * perPage;
+    let array = products.value().slice(start, end);
+    res.render('product/index_product', { _listProducts: array });
 }
--- views/product/product.js
+++ views/product/product.js
@@ -4,5 +4,8 @@ const controllerProduct = require('./product.controllers');
 const router = express.Router();
 
 router.get('/', controllerProduct.index);
+router.get('/:page', controllerProduct.index);
+// sử dụng router kiểu này thì muốn lấy giá trị ra phải dùng params
+// còn sử dụng ? trên router thì phải sử dụng query
 
 module.exports = router;
